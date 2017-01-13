b822dfe1fb58bc70a16bc2e1dc8e8e9c6284b69c
Rutvij <rutvij.shah@live.com>
2017-01-13 01:05:05 +0000

    initial commit

--- highest_number_cubed/main.py
+++ highest_number_cubed/main.py
@@ -2,4 +2,45 @@
 
 
 def highest_number_cubed(limit):
-    pass
+    previous_number = 1
+    
+    while True:
+        current_number = previous_number + 1
+        if current_number ** 3 > limit:
+            return previous_number
+            
+        previous_number = current_number
+        
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ 
+ #   i = 1
+  
+ #  while i < limit:
+  #      y = i ** 3
+   #     if y < limit:
+    #        i += 1
+     
+     #   else:
+      
+      #  break
+    #return y
+    
+    
+
+# limit = 30
+# 1 < 30 line6
+# 1
